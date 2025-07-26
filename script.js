1d85aa0616b045bf1e21114346cd7ce3f0e87fb3
faizan <rfaizan15@gmail.com>
2025-07-26 12:50:59 +0500

    All Game Mechanics Completed cba with the styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Gameboard = (function () {
   let gameboard = [];
   let dim = 3;
-  let win = true;
+  let win = false;
   let tie = false;
 
   for (let i = 0; i < dim; i++) {
@@ -14,18 +14,21 @@ const Gameboard = (function () {
   function checkGameFinish() {
     function checkWin(i, j) {
       const currPiece = gameboard[i][j];
+
       function checkHorizontal() {
         for (let x = 0; x < dim; x++) {
           if (gameboard[i][x] != currPiece) return false;
         }
         return true;
       }
+
       function checkVertical() {
         for (let x = 0; x < dim; x++) {
           if (gameboard[x][j] != currPiece) return false;
         }
         return true;
       }
+
       function checkDiagonal() {
         for (let x = 0; x < dim; x++) {
           if (gameboard[x][x] != currPiece) return false;
@@ -34,12 +37,12 @@ const Gameboard = (function () {
       }
 
       if (checkVertical(i, j) || checkHorizontal(i, j) || checkDiagonal(i, j)) {
-        console.log(`Player ${gameboard[i][j]} has won the match!`);
         return true;
       }
       return false;
     }
 
+    tie = true;
     for (let i = 0; i < dim; i++) {
       for (let j = 0; j < dim; j++) {
         let currentPiece = gameboard[i][j];
@@ -51,13 +54,6 @@ const Gameboard = (function () {
         if (currentPiece == ".") tie = false;
       }
       if (win) break;
-    }
-
-    if (win || tie) {
-      const gameBoxes = document.querySelectorAll(".game-div");
-      gameBoxes.forEach((e) => {
-        e.removeEventListener("click", () => {});
-      });
     }
   }
 
@@ -74,23 +70,63 @@ const Gameboard = (function () {
     checkGameFinish();
   }
 
-  return { gameboard, playerMove, checkGameFinish, display, win, tie };
+  function getWin() {
+    return win;
+  }
+
+  function getTie() {
+    return tie;
+  }
+
+  function restart() {
+    tie = false;
+    win = false;
+
+    for (let i = 0; i < dim; i++) {
+      gameboard[i] = [];
+      for (let j = 0; j < dim; j++) {
+        gameboard[i][j] = ".";
+      }
+    }
+  }
+
+  return { gameboard, playerMove, checkGameFinish, display, getWin, getTie, restart };
 })();
 
 const displayController = (function () {
   let currType = "X";
+  let xName = "";
+  let oName = "";
+
+  // Name getting stuff
+  const submitDialogbtn = document.querySelector(".dialogsubmit-btn");
+  const nameDialog = document.querySelector(".addname-dialog");
+
+  submitDialogbtn.addEventListener("click", (e) => {
+    const xInput = document.querySelector('[name="xName"');
+    const oInput = document.querySelector('[name="oName"');
+    xName = xInput.value;
+    oName = oInput.value;
+
+    if (xName && oName) {
+      e.preventDefault();
+      nameDialog.close();
+    }
+  });
 
   function updatePlayerPos(x, y) {
-    console.log(String(x) + String(y));
     const gameBox = document.querySelector(
       `[data-xy="${String(x) + String(y)}"]`
     );
+
     if (gameBox.textContent != "") return;
 
     gameBox.textContent = currType;
   }
 
   const gameBoxes = document.querySelectorAll(".game-div");
+  const endMsg = document.querySelector(".end-msg");
+
   gameBoxes.forEach((e) => {
     e.addEventListener("click", () => {
       let coords = e.dataset.xy;
@@ -99,16 +135,37 @@ const displayController = (function () {
 
       updatePlayerPos(xCord, yCord);
       Gameboard.playerMove(xCord, yCord, currType);
+      const endMsg = document.querySelector(".end-msg");
+
+      if (Gameboard.getWin()) {
+        let winnerName = currType === "X" ? xName : oName;
+        endMsg.textContent = `${winnerName} has won the match.`;
+
+        gameBoxes.forEach((box) => {
+          box.removeEventListener("click", () => {});
+        });
+      }
+
+      if (Gameboard.getTie()) {
+        endMsg.textContent = `The Match has been tied`;
+      }
+
       currType = currType === "X" ? "O" : "X";
-
-      if (Gameboard.win) {
-
-      }
-
-      if (Gameboard.tie) {
-        
-      }
     });
+  });
+
+  // Restart button Stuff
+  const restartBtn = document.querySelector(".restart-btn");
+
+  restartBtn.addEventListener("click", (event) => {
+    Gameboard.restart();
+
+    const gameBoxes = document.querySelectorAll(".game-div");
+    gameBoxes.forEach((e) => {
+      e.textContent = "";
+    });
+
+    endMsg.textContent = "";
   });
 
   return { updatePlayerPos };
